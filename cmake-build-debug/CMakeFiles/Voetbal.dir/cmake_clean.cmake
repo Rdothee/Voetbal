6c99dd7eeee78348343413f4e03872fe263e749e
Rdothee <robbe.dothee@hotmail.com>
2019-11-18 11:48:24 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/Voetbal.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Voetbal.dir/main.cpp.o"
+  "Voetbal.exe"
+  "Voetbal.pdb"
+  "libVoetbal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Voetbal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
